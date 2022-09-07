aee2f2501e15547854163e05374a771cbfcbebfd
Zachary Maillard <zmaillard@protonmail.com>
2022-09-06 20:21:28 -0600

    Remove obsolete

--- assets/js/carousel.js
+++
@@ -1,7 +0,0 @@
-import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.js';
-
-// Initialize all elements with carousel class.
-bulmaCarousel.attach('.carousel', {
-    slidesToScroll: 1,
-    slidesToShow: 4
-});
